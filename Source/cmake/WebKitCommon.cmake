ff2b8ffd9e96bbefa3cfc1d6bb00b6a1c2149362
Bala Avulapati <bavulapati@gmail.com>
2023-12-14 16:47:07 +0530

    Reapply "Integrate vcpkg"
    This reverts commit f388ca1d675100580cb7082ae3797818cc1cc4c7.

--- Source/cmake/WebKitCommon.cmake
+++ Source/cmake/WebKitCommon.cmake
@@ -226,7 +226,6 @@ if (NOT HAS_RUN_WEBKIT_COMMON)
     include(WebKitCompilerFlags)
     include(WebKitStaticAnalysis)
     include(WebKitFeatures)
-    include(WebKitFindPackage)
 
     include(OptionsCommon)
     include(Options${PORT})
